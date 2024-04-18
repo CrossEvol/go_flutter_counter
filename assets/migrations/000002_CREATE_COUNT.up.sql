423a9a7b3ca5168824dd47432cc78dad60f606f8
创圣大天使Evol <296662402@qq.com>
2024-04-18 10:37:12 +0800

    db_test :: exec

--- assets/migrations/000002_CREATE_COUNT.up.sql
+++ assets/migrations/000002_CREATE_COUNT.up.sql
@@ -1,13 +1,15 @@
-CREATE TABLE COUNT (
-                       key VARCHAR(50) PRIMARY KEY ,
-                       value INT  NOT NULL DEFAULT 0,
-                       updated_at  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE COUNT
+(
+    key        VARCHAR(50) PRIMARY KEY,
+    value      INT      NOT NULL DEFAULT 0,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE COUNT_RECORD(
-                             id INT PRIMARY KEY,
-                             key VARCHAR(50) NOT NULL  DEFAULT  '',
-                             old_value INT,
-                             new_value INT,
-                             created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE COUNT_RECORD
+(
+    id         INTEGER PRIMARY KEY,
+    key        VARCHAR(50) NOT NULL DEFAULT '',
+    old_value  INT,
+    new_value  INT,
+    created_at DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
--- internal/database/db_test.go
+++ internal/database/db_test.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"github.com/crossevol/go_flutter_counter/internal/database/dao"
 	_ "github.com/mattn/go-sqlite3" // Import the Sqlite driver
@@ -43,21 +44,47 @@ func (suite *DatabaseTestSuite) TearDownTest() {
 }
 
 func (suite *DatabaseTestSuite) TestDatabase() {
+	testCount(suite)
+	testCountRecord(suite)
+}
+
+func testCount(suite *DatabaseTestSuite) {
 	_, err := queries.InitCountValue(ctx)
 	require.Nil(suite.T(), err)
 	countValue, err := queries.GetCountValue(ctx)
 	require.Nil(suite.T(), err)
-	require.Zero(suite.T(), countValue)
+	require.Equal(suite.T(), 0, int(countValue))
 	err = queries.IncrementCountValue(ctx)
 	require.Nil(suite.T(), err)
 	countValue, err = queries.GetCountValue(ctx)
 	require.Nil(suite.T(), err)
 	require.Equal(suite.T(), 1, int(countValue))
+	err = queries.ResetCountValue(ctx)
+	require.Nil(suite.T(), err)
+	countValue, err = queries.GetCountValue(ctx)
+	require.Nil(suite.T(), err)
+	require.Equal(suite.T(), 0, int(countValue))
 	err = queries.DecrementCountValue(ctx)
 	require.Nil(suite.T(), err)
 	countValue, err = queries.GetCountValue(ctx)
 	require.Nil(suite.T(), err)
-	require.Zero(suite.T(), countValue)
+	require.Equal(suite.T(), -1, int(countValue))
+}
+
+func testCountRecord(suite *DatabaseTestSuite) {
+	createCounterRecord, err := queries.CreateCounterRecord(ctx, sql.NullInt64{Valid: true, Int64: 1})
+	require.Nil(suite.T(), err)
+	lastInsertId, err := createCounterRecord.LastInsertId()
+	require.Nil(suite.T(), err)
+	require.Equal(suite.T(), 1, int(lastInsertId))
+	counterRecords, err := queries.GetCounterRecords(ctx)
+	require.Nil(suite.T(), err)
+	require.Equal(suite.T(), 1, len(counterRecords))
+	require.Equal(suite.T(), "counter", counterRecords[0].Key)
+	require.Equal(suite.T(), 1, int(counterRecords[0].NewValue.Int64))
+	count, err := queries.CountCounterRecords(ctx)
+	require.Nil(suite.T(), err)
+	require.Equal(suite.T(), 1, int(count))
 }
 
 func TestDatabaseTestSuite(t *testing.T) {
